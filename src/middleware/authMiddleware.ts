65e54f0cffa51bb51b7a2896143c376ed36d6df2
ashishkunthe <ashishkunthe2003@gmail.com>
2025-06-11 15:25:38 +0530

    auth middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,5 +1,35 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-function authMiddleware(req: Request, res: Response, next: NextFunction) {}
+dotenv.config();
+
+declare module "express-serve-static-core" {
+  interface Request {
+    userId?: string;
+  }
+}
+
+function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    res.json({
+      message: "token is not provided",
+    });
+    return;
+  }
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET as string) as {
+      userId: string;
+    };
+
+    req.userId = decode.userId;
+    next();
+  } catch (e) {
+    res.json({ message: "something went wrong please try later" });
+  }
+}
 
 export default authMiddleware;
